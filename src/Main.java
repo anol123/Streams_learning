a8edb732c3e8eb481783e8041199aca0c59500d7
anol123 <anol.roy123@gmail.com>
2026-09-17 22:59:02 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ public class Main {
 
         List<Integer> evenList = list.stream().filter(n->n%2==0).collect(Collectors.toList());
         System.out.println(evenList);
+
+        List<Integer> oddList = list.stream().filter(n->n%2 !=0).collect(Collectors.toList());
+
+        System.out.println(oddList);
     }
 }
