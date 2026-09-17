ec79fe89f405d5636894933dfd9c7290c3564dec
anol123 <anol.roy123@gmail.com>
2026-09-17 22:57:26 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(10,20,35,45,53,60);
+
+        List<Integer> evenList = list.stream().filter(n->n%2==0).collect(Collectors.toList());
+        System.out.println(evenList);
     }
 }
