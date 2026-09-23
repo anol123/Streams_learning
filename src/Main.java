0634846547fd7d9a8e30f4100a4912253077aef3
anol123 <anol.roy123@gmail.com>
2026-09-23 22:34:36 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(10,20,35,45,53,60);
 
-        List<Integer> evenList = list.stream().filter(n->n%2==0).collect(Collectors.toList());
-        System.out.println(evenList);
+//        List<Integer> evenList = list.stream().filter(n->n%2==0).collect(Collectors.toList());
+//        System.out.println(evenList);
 
         List<Integer> oddList = list.stream().filter(n->n%2 !=0).collect(Collectors.toList());
 
