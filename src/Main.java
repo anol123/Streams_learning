243fb2cb0f2ecf96415dbf1f1b8cfaab3f976e65
anol123 <anol.roy123@gmail.com>
2026-09-24 22:51:45 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(10,20,35,45,53,60);
+        List<Integer> list = Arrays.asList(10, 15, 20, 25, 30, 35, 40, 45, 50);
 
         List<Integer> evenList = list.stream().filter(n->n%2==0).collect(Collectors.toList());
         System.out.println(evenList);
