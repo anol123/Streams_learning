dc2f0a94b236440ce62737bb53a23505579d35ba
anol123 <anol.roy123@gmail.com>
2026-09-25 22:12:31 +0530

    working

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
         System.out.println(oddList);
 
 
-//        List<Integer> greaterThan30 = list.stream().filter(n->n>30).collect(Collectors.toList());
-//        System.out.println(greaterThan30);
+        List<Integer> greaterThan30 = list.stream().filter(n->n>30).collect(Collectors.toList());
+        System.out.println(greaterThan30);
 
         List<Integer> greaterThan20AndEven = list.stream().filter(n->n>20).filter(n-> n%2==0).collect(Collectors.toList());
 
